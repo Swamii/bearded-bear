bf5e3fe209e91b89a18c817315d9170aad827443
Akseli Nelander <swami@swami-ubuntu.(none)>
2012-08-20 20:59:37 +0200

    small changes to the blog app

--- deejango/deejango/blog/views.py
+++ deejango/deejango/blog/views.py
@@ -1,10 +1,22 @@
+from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from deejango.blog.models import Post
 
 def posts_all(request):
-	post = Post.objects.all().order_by('-created')
-	context = {'post': post}
+	posts = Post.objects.all().order_by('-created')
+	paginator = Paginator(posts, 2)
+	page = request.GET.get('page')
+
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+
+	context = {'posts': posts}
 
 	return render_to_response('blog.html', context, RequestContext(request))
 
